3a561338ae997ccad8f16c44cb6d377bab351cd7
Dennis Elser <patois@users.noreply.github.com>
2020-11-18 19:52:29 +0100

    Add files via upload

--- A5Pack.py
+++ A5Pack.py
@@ -7,13 +7,26 @@ class A5Pack:
     HDR_SIZE = calcsize(HDR_FMT)
     INFO_CHUNK_FMT = "<20s20sI"
     INFO_CHUNK_SIZE = calcsize(INFO_CHUNK_FMT)
+    HDR_MAGIC = b"A5Pack"
+    VALID_CHUNKS = {
+        "A5 code":(0x40020000, 0x132000),
+        "A5 voice":(0x40200000, 0x5000000),
+        "A5 Reserve1":(0x479C0000, 0x100000),
+        "A5 Reserve2":(0x47AC0000, 0x100000),
+        "A5 Reserve3":(0x47BC0000, 0x100000),
+        "A5 patch":(0x47F00000, 0x0A5400),
+        "A5 patch(5)":(0x47E80000, 0x14A800),
+        "stm32 code":(0x8010000, 0x70000),
+        "stm32F030 code":(0x8002800, 0x0D800),
+        "Z-A5 bootloader":(0x40000000, 0x20000)
+    }
 
     def __init__(self, buf):
         self.buf = buf
         return
 
     def read_header(self):
-        """returns tuple: signature and model id"""
+        """returns tuple: magic and revision id"""
         return unpack(A5Pack.HDR_FMT, self.buf[:A5Pack.HDR_SIZE])
 
     def get_first_file(self):
@@ -52,31 +65,38 @@ buf = f.read()
 f.close()
 
 a5 = A5Pack(buf)
-r = a5.get_first_file()
-if r:
-    file_idx = 0
-    ver, tag, offs, size = r
-    name = "%s_%d_%08x_%s" % (ver.decode("utf-8").strip("\x00"),
-                            file_idx,
-                            offs,
-                            tag.decode("utf-8").strip("\x00").replace(" ", "_"))
-    print("%s 0x%x %d" % (name, offs, size))
-    f = open(name, "wb")
-    f.write(a5.extract_file(offs, size))
-    f.close()
-    while True:
-        r = a5.get_next_file()
-        if r:
-            file_idx += 1
-            ver, tag, offs, size = r
-            name = "%s_%d_%08x_%s" % (ver.decode("utf-8").strip("\x00"),
-                                    file_idx,
-                                    offs,
-                                    tag.decode("utf-8").strip("\x00").replace(" ", "_"))
-            print("%s 0x%x %d" % (name, offs, size))
-            f = open(name, "wb")
-            f.write(a5.extract_file(offs, size))
-            f.close()
-        else:
-            print("No more files. %d files in total." % (file_idx+1))
-            break
+magic, revid = a5.read_header()
+if magic == A5Pack.HDR_MAGIC:
+    revision = revid[1:].decode("utf-8")
+    r = a5.get_first_file()
+    if r:
+        file_idx = 0
+        ver, tag, offs, size = r
+        name = "%s%s_%d_%08x_%s" % (
+            revision,
+            ver.decode("utf-8").strip("\x00"),
+            file_idx,
+            offs,
+            tag.decode("utf-8").strip("\x00").replace(" ", "_"))
+        print("%s 0x%x %d" % (name, offs, size))
+        f = open(name, "wb")
+        f.write(a5.extract_file(offs, size))
+        f.close()
+        while True:
+            r = a5.get_next_file()
+            if r:
+                file_idx += 1
+                ver, tag, offs, size = r
+                name = "%s%s_%d_%08x_%s" % (
+                    revision,
+                    ver.decode("utf-8").strip("\x00"),
+                    file_idx,
+                    offs,
+                    tag.decode("utf-8").strip("\x00").replace(" ", "_"))
+                print("%s 0x%x %d" % (name, offs, size))
+                f = open(name, "wb")
+                f.write(a5.extract_file(offs, size))
+                f.close()
+            else:
+                print("No more files. %d files in total." % (file_idx+1))
+                break
